305d822f0ebdff3371ceb428880629609a315c32
Felipe Andrés Vásquez Tort <f.vasquez.tort@gmail.com>
2024-09-01 11:37:59 -0400

    Entregas hasta la sesion 7 del modulo 4

---
+++ Modulo_4/src/main/java/com/bufigol/s7/interfaces/InterfazVehiculo.java
@@ -0,0 +1,7 @@
+package com.bufigol.s7.interfaces;
+
+public interface InterfazVehiculo {
+    public  String tipoDeBencina();
+
+    public  void tipoDeBencina(String tipoDeBencina);
+}
---
+++ Modulo_4/src/main/java/com/bufigol/s7/interfaces/Productointerface.java
@@ -0,0 +1,10 @@
+package com.bufigol.s7.interfaces;
+
+public interface Productointerface {
+
+    public void cambiarPrecio(int precio);
+
+    public int cantidadDisponible();
+
+    public void cantidadDisponible(int disponible);
+}
