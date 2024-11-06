12dae054d02d292b4df6d2739d7430f8f1539931
Bufigol <f.vasquez.tort@gmail.com>
2024-11-06 18:19:20 -0300

    Comienzo desarrollo evaluacionb modulo 6 sesion 2

---
+++ Modulo 6/EVM6S2/src/main/java/com/bufigol/evm6s2/Evm6S2Application.java
@@ -0,0 +1,13 @@
+package com.bufigol.evm6s2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evm6S2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Evm6S2Application.class, args);
+    }
+
+}
