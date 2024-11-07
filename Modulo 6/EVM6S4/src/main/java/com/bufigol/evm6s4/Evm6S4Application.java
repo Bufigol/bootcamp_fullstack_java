21f2ef8f03dc17d91ef23d26680a985d8079e1c7
Bufigol <f.vasquez.tort@gmail.com>
2024-11-07 18:25:57 -0300

    Comienzo desarrollo Evaluacion Modulo 6 Sesion 4

---
+++ Modulo 6/EVM6S4/src/main/java/com/bufigol/evm6s4/Evm6S4Application.java
@@ -0,0 +1,13 @@
+package com.bufigol.evm6s4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evm6S4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Evm6S4Application.class, args);
+    }
+
+}
