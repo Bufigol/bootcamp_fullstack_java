7e38e5dafc4e5027e4b893bc9bd85bbbe2f7df0d
Bufigol <f.vasquez.tort@gmail.com>
2024-11-07 16:02:02 -0300

    Comienzo de desarrollo EX Modulo 6 Sesion 4

---
+++ Modulo 6/ECM6S4/src/test/java/com/bufigol/ecm6s4/Ecm6S4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bufigol.ecm6s4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecm6S4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
