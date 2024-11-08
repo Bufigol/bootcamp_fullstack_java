b9d74c7372d1f7fbe5bd4e3b01eff84e11068457
Bufigol <f.vasquez.tort@gmail.com>
2024-11-08 16:05:37 -0300

    Fin sesion 6

---
+++ Modulo 6/EVM6S5/src/test/java/com/bufigol/evm6s5/Evm6S5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bufigol.evm6s5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evm6S5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
